cb9126f0c3b781dfcebf6219c21dbff383ea604a
Fausto <faustocamp545@gmail.com>
2022-03-21 17:21:28 -0500

    Diseño del login

---
+++ app/src/main/java/com/ruta_del_valle/app_hosteria/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ruta_del_valle.app_hosteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
