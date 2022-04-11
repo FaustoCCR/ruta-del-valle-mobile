7e8cdcc573d2cb3e44afaf83e299efae307b930a
Fausto <faustocamp545@gmail.com>
2022-04-11 13:37:21 -0500

    Validaciones Date Pickers

--- app/src/main/java/com/ruta_del_valle/app_hosteria/fragments/ReservaFragment.java
+++ app/src/main/java/com/ruta_del_valle/app_hosteria/fragments/ReservaFragment.java
@@ -23,9 +23,9 @@ import java.util.Date;
 
 public class ReservaFragment extends Fragment implements View.OnClickListener {
 
-    EditText editTextFecha;
-    ImageButton btnFecha;
-    DatePicker dpFecha;
+    EditText editTextFechaEntrada,editTextFechaSalida;
+    ImageButton btnFechaEntrada, btnFechaSalida;
+    DatePicker dpFechaEntrada, dpFechaSalida;
 
     Calendar calendar;
 
@@ -43,48 +43,124 @@ public class ReservaFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_reserva, container, false);
 
         /*Conectamos con los componentes gráficos*/
-        editTextFecha = view.findViewById(R.id.editTextDate);
-        btnFecha = view.findViewById(R.id.imageButtonDate1);
-        dpFecha = view.findViewById(R.id.datePicker1);
+        editTextFechaEntrada = view.findViewById(R.id.etFechaEntrada);
+        editTextFechaSalida = view.findViewById(R.id.etFechaSalida);
+        btnFechaEntrada = view.findViewById(R.id.iBFechaEntrada);
+        btnFechaSalida = view.findViewById(R.id.iBFechaSalida);
+        dpFechaEntrada = view.findViewById(R.id.datePicker1);
+        dpFechaSalida = view.findViewById(R.id.datePicker2);
 
-        btnFecha.setOnClickListener(this);
+        btnFechaEntrada.setOnClickListener(this);
+        btnFechaSalida.setOnClickListener(this);
 
         calendar = Calendar.getInstance();
 
-        dpFecha.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+        /*
+        * Date Picker - Entrada
+        * */
+        dpFechaEntrada.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
-                editTextFecha.setText(getFechaDatePicker());
-                dpFecha.setVisibility(View.GONE);
+                editTextFechaEntrada.setText(getFechaDatePickerEntrada());
+                dpFechaEntrada.setVisibility(View.GONE);
+                checkPickerEntradaToChangePickerSalida();
+                //setMinDateSalida();
+
+            }
+        });
+        /*
+         * Date Picker - Salida
+         * */
+        dpFechaSalida.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)+1, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                editTextFechaSalida.setText(getFechaDatePickerSalida());
+                dpFechaSalida.setVisibility(View.GONE);
             }
         });
 
-        editTextFecha.setText(getFechaDatePicker());
+        //restricciones datePickers
+        dpFechaEntrada.setMinDate(calendar.getTimeInMillis());
+        dpFechaSalida.setMinDate(setMinDateSalida().getTimeInMillis());
+        //---------------
+        editTextFechaEntrada.setText(getFechaDatePickerEntrada());
+        editTextFechaSalida.setText(getFechaDatePickerSalida());
 
         return view;
     }
 
+    protected Calendar setMinDateSalida(){
 
-    protected String getFechaDatePicker(){
+        Calendar fechaSalida = Calendar.getInstance();
+        fechaSalida.set(dpFechaEntrada.getYear(),dpFechaEntrada.getMonth(),dpFechaEntrada.getDayOfMonth()+1);
+        return fechaSalida;
+    }
 
-        String dia = String.valueOf(dpFecha.getDayOfMonth());
-        String mes = String.valueOf(dpFecha.getMonth()+1);
-        String anio = String.valueOf(dpFecha.getYear());
+    protected String getFechaDatePickerEntrada(){
+
+        String dia = String.valueOf(dpFechaEntrada.getDayOfMonth());
+        String mes = String.valueOf(dpFechaEntrada.getMonth()+1);
+        String anio = String.valueOf(dpFechaEntrada.getYear());
 
         return dia+"/"+mes+"/"+anio;
 
     }
 
-    /*Mostrar Calendario*/
-    protected void showCalendar(){
+    protected String getFechaDatePickerSalida(){
 
-        dpFecha.setVisibility(View.VISIBLE);
+        String dia = String.valueOf(dpFechaSalida.getDayOfMonth());
+        String mes = String.valueOf(dpFechaSalida.getMonth()+1);
+        String anio = String.valueOf(dpFechaSalida.getYear());
+
+        return dia+"/"+mes+"/"+anio;
+    }
+
+    protected void checkPickerEntradaToChangePickerSalida(){
+        Calendar fechaEntrada = Calendar.getInstance();
+        fechaEntrada.set(dpFechaEntrada.getYear(),dpFechaEntrada.getMonth(),dpFechaEntrada.getDayOfMonth());
+
+        Calendar fechaSalida = Calendar.getInstance();
+        fechaSalida.set(dpFechaSalida.getYear(),dpFechaSalida.getMonth(),dpFechaSalida.getDayOfMonth());
+
+        if (fechaEntrada.getTime().equals(fechaSalida.getTime()) || fechaSalida.getTime().before(fechaEntrada.getTime())){
+
+            int dayMonth = dpFechaEntrada.getDayOfMonth() + 1;
+            int month = dpFechaEntrada.getMonth();
+            int year = dpFechaEntrada.getYear();
+
+            String dia = String.valueOf(dayMonth);
+            String mes = String.valueOf(month+1);
+            String anio = String.valueOf(year);
+
+            String date = dia+"/"+mes+"/"+anio;
+
+            editTextFechaSalida.setText(date);
+            dpFechaSalida.updateDate(year,month,dayMonth);
+
+        }
+    }
+
+    /*Mostrar Calendario*/
+    protected void showCalendar(DatePicker datePicker){
+
+        datePicker.setVisibility(View.VISIBLE);
 
 
     }
 
     @Override
-    public void onClick(View view) {
-        showCalendar();
+    public void onClick(View view)
+    {
+
+        switch (view.getId()){
+            case R.id.iBFechaEntrada:
+                showCalendar(dpFechaEntrada);
+            break;
+
+            case R.id.iBFechaSalida:
+                showCalendar(dpFechaSalida);
+            break;
+
+        }
     }
 }
